64f4701f5724dfe5f92c6daae150995ae6b079d7
rusfam <samuel.rufinatscha@iota.org>
2021-02-10 20:11:50 +0100

    add construction submit

--- src/construction.rs
+++ src/construction.rs
@@ -5,7 +5,10 @@ use crate::{
     error::ApiError,
     filters::{handle, with_options},
     options::Options,
-    types::{ConstructionHashRequest, ConstructionHashResponse, TransactionIdentifier},
+    types::{
+        ConstructionHashRequest, ConstructionHashResponse, ConstructionSubmitRequest, ConstructionSubmitResponse,
+        TransactionIdentifier,
+    },
 };
 use bee_common::packable::Packable;
 use iota::Message;
@@ -13,12 +16,17 @@ use log::debug;
 use warp::Filter;
 
 pub fn routes(options: Options) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone {
-    warp::post().and(
-        warp::path!("construction" / "hash")
+    warp::post()
+        .and(
+            warp::path!("construction" / "hash")
+                .and(warp::body::json())
+                .and(with_options(options.clone()))
+                .and_then(handle(construction_hash_request)),
+        )
+        .or(warp::path!("construction" / "submit")
             .and(warp::body::json())
             .and(with_options(options.clone()))
-            .and_then(handle(construction_hash_request)),
-    )
+            .and_then(handle(construction_submit_request)))
 }
 
 async fn construction_hash_request(
@@ -26,19 +34,49 @@ async fn construction_hash_request(
     options: Options,
 ) -> Result<ConstructionHashResponse, ApiError> {
     debug!("/construction/hash");
-    match hex::decode(construction_hash_request.signed_transaction) {
-        Ok(bytes) => {
-            let message = Message::unpack(&mut bytes.as_slice()).map_err(|_| {
-                ApiError::BadConstructionRequest("can not build message from signed_transaction".to_string())
-            })?;
-            Ok(ConstructionHashResponse {
-                transaction_identifier: TransactionIdentifier {
-                    hash: message.id().0.to_string(),
-                },
-            })
-        }
+    let message = message_from_hex(&construction_hash_request.signed_transaction)?;
+    Ok(ConstructionHashResponse {
+        transaction_identifier: TransactionIdentifier {
+            hash: message.id().0.to_string(),
+        },
+    })
+}
+
+async fn construction_submit_request(
+    construction_submit_request: ConstructionSubmitRequest,
+    options: Options,
+) -> Result<ConstructionSubmitResponse, ApiError> {
+    debug!("/construction/submit");
+    let message = message_from_hex(&construction_submit_request.signed_transaction)?;
+
+    let iota_client = match iota::Client::builder()
+        .with_network(&options.network)
+        .with_node(&options.iota_endpoint)
+        .unwrap()
+        .with_node_sync_disabled()
+        .finish()
+        .await
+    {
+        Ok(iota_client) => iota_client,
+        Err(_) => return Err(ApiError::UnableToBuildClient),
+    };
+
+    match iota_client.post_message(&message).await {
+        Ok(message_id) => Ok(ConstructionSubmitResponse {
+            transaction_identifier: TransactionIdentifier {
+                hash: message_id.to_string(),
+            },
+        }),
+        Err(_) => Err(ApiError::BadConstructionRequest("can not submit message".to_string())),
+    }
+}
+
+fn message_from_hex(hex_str: &str) -> Result<Message, ApiError> {
+    match hex::decode(hex_str) {
+        Ok(bytes) => Ok(Message::unpack(&mut bytes.as_slice())
+            .map_err(|_| ApiError::BadConstructionRequest("can not build message from hex string".to_string()))?),
         Err(e) => Err(ApiError::BadConstructionRequest(
-            "can not decode signed_transaction, invalid hex string".to_string(),
+            "can not build message from hex string".to_string(),
         )),
     }
 }
