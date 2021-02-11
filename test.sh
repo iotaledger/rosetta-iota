ad4b391f9e50b4ce0da23f1ea601adac9e560d72
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2021-02-10 23:25:13 -0300

    pruning index on test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
 
+NODE_URL="http://honeycombos.iota.cafe:14265"
+NETWORK="testnet4"
+
 # start server
-RUST_LOG=iota_rosetta=debug cargo run -- --network alphanet1 --iota-endpoint http://0.0.0.0:14265 &
+RUST_LOG=iota_rosetta=debug cargo run -- --network $NETWORK --iota-endpoint $NODE_URL &
 PID=$!
 
+# wait for server to completely start
 sleep 1
+
+# modify rosetta-iota.json
+PRUNE_MS=$(curl -X GET "$NODE_URL/api/v1/info" -H  "accept: application/json" | jq '.data.pruningIndex')
+START_MS=`expr $PRUNE_MS + 1`
+
+cat <<< $(jq --argjson START_MS "$START_MS" '.data.start_index |= $START_MS' rosetta-iota.json) > rosetta-iota.json
+cat <<< $(jq --arg NETWORK "$NETWORK" '.network.network |= $NETWORK' rosetta-iota.json) > rosetta-iota.json
 
 # test Data API
 ~/bin/rosetta-cli check:data --configuration-file rosetta-iota.json
 
 # test Construction API
-~/bin/rosetta-cli check:construction --configuration-file rosetta-iota.json
+# ~/bin/rosetta-cli check:construction --configuration-file rosetta-iota.json
 
 kill $PID
