8748bb4d38e4be8b2b91afec1a8cfca170bf864f
Bernardo A. Rodrigues <bernardo.araujo@iota.org>
2021-03-04 22:51:40 -0300

    prefunded_accounts via rosetta_cli.sh

--- rosetta_cli.sh
+++ rosetta_cli.sh
@@ -4,10 +4,13 @@ NODE_URL="http://honeycombos.iota.cafe:14265"
 NETWORK="testnet6"
 DATA_DIR=".rosetta-cli"
 
+ROOT=$(pwd)
+
 # 1 to enable, comment out to disable
 PRUNE=1
 #RECONCILE=1
 CLEAN=1
+PREFUNDED_ACCOUNT=1
 
 # start server
 RUST_LOG=iota_rosetta=debug cargo run -- --network $NETWORK --iota-endpoint $NODE_URL --port 3030 --mode online &
@@ -20,42 +23,59 @@ if [ $CLEAN ]; then
   rm -rf $DATA_DIR
 fi
 
+if [ $PREFUNDED_ACCOUNT ]; then
+
+  echo "asking for faucet funds to load up prefunded_accounts..."
+
+  cd src/utils
+  PREFUNDED_ACCOUNT=$(cargo run)
+
+  SK=$(echo $PREFUNDED_ACCOUNT | jq '.sk')
+  ADDR=$(echo $PREFUNDED_ACCOUNT | jq '.bech32_addr')
+
+  cat <<< $(jq --argjson ADDR "$ADDR" '.construction.prefunded_accounts[0].account_identifier.address |= $ADDR' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq --argjson SK "$SK" '.construction.prefunded_accounts[0].privkey |= $SK' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+
+  cd $ROOT
+
+fi
+
 if [ $PRUNE ]; then
 
   # modify rosetta-iota.json to make sure we are syncing from the pruned milestone
   PRUNE_MS=$(curl -X GET "$NODE_URL/api/v1/info" -H  "accept: application/json" | jq '.data.pruningIndex')
   START_MS=`expr $PRUNE_MS + 2`
 
-  cat <<< $(jq --argjson START_MS "$START_MS" '.data.start_index |= $START_MS' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-  cat <<< $(jq '.data.pruning_disabled |= false' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq --argjson START_MS "$START_MS" '.data.start_index |= $START_MS' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.pruning_disabled |= false' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 else
 
-  cat <<< $(jq 'del(.data.start_index)' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-  cat <<< $(jq '.data.pruning_disabled |= true' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq 'del(.data.start_index)' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.pruning_disabled |= true' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 fi
 
 if [ $RECONCILE ]; then
 
-  cat <<< $(jq '.data.reconciliation_disabled |= false' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-  cat <<< $(jq '.data.end_conditions.reconciliation_coverage.coverage |= 0.95' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-  cat <<< $(jq '.data.end_conditions.reconciliation_coverage.from_tip |= true' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.reconciliation_disabled |= false' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.end_conditions.reconciliation_coverage.coverage |= 0.95' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.end_conditions.reconciliation_coverage.from_tip |= true' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 else
 
-  cat <<< $(jq '.data.reconciliation_disabled |= true' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-  cat <<< $(jq 'del(.data.end_conditions.reconciliation_coverage)' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq '.data.reconciliation_disabled |= true' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+  cat <<< $(jq 'del(.data.end_conditions.reconciliation_coverage)' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 fi
 
-cat <<< $(jq --arg NETWORK "$NETWORK" '.network.network |= $NETWORK' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
-cat <<< $(jq --arg DATA_DIR "$DATA_DIR" '.data_directory |= $DATA_DIR' rosetta-cli-conf/rosetta-iota.json) > rosetta-cli-conf/rosetta-iota.json
+cat <<< $(jq --arg NETWORK "$NETWORK" '.network.network |= $NETWORK' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
+cat <<< $(jq --arg DATA_DIR "$DATA_DIR" '.data_directory |= $DATA_DIR' $ROOT/rosetta-cli-conf/rosetta-iota.json) > $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 # test Data API
-~/bin/rosetta-cli check:data --configuration-file rosetta-cli-conf/rosetta-iota.json
+~/bin/rosetta-cli check:data --configuration-file $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 # test Construction API
-~/bin/rosetta-cli check:construction --configuration-file rosetta-cli-conf/rosetta-iota.json
+~/bin/rosetta-cli check:construction --configuration-file $ROOT/rosetta-cli-conf/rosetta-iota.json
 
 kill $PID
