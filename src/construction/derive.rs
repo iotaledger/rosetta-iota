23948bd38fdb48af32da8833619bb0e41c0e84b0
rusfam <samuel.rufinatscha@iota.org>
2021-03-09 17:33:19 +0100

    refactor derive.rs

--- src/construction/derive.rs
+++ src/construction/derive.rs
@@ -39,7 +39,7 @@ pub async fn construction_derive_request(
     let public_key_bytes = hex::decode(construction_derive_request.public_key.hex_bytes)?;
     let public_key_hash = Blake2b256::digest(&public_key_bytes);
 
-    let address = Address::Ed25519(Ed25519Address::new(public_key_hash.try_into().unwrap()));
+    let address = Address::Ed25519(Ed25519Address::new(public_key_hash.into()));
 
     // todo: treat timeout on this unrwap
     let bech32_hrp = iota_client.get_bech32_hrp().await.unwrap();
@@ -54,38 +54,32 @@ mod tests {
 
     use super::*;
 
-    fn default_network_identifier() -> NetworkIdentifier {
-        NetworkIdentifier {
-            blockchain: "iota".to_string(),
-            network: "testnet5".to_string(),
-            sub_network_identifier: None
-        }
-    }
-
-    fn default_options() -> Options {
-        Options {
-            iota_endpoint: "https://api.lb-0.testnet.chrysalis2.com".to_string(),
-            network: "testnet5".to_string(),
-            mode: "online".to_string(),
-            port: 3030
-        }
-    }
-
     #[tokio::test]
     async fn test_address_from_public_key() {
         let request = ConstructionDeriveRequest {
-            network_identifier: default_network_identifier(),
+            network_identifier: NetworkIdentifier {
+                blockchain: "iota".to_string(),
+                network: "testnet6".to_string(),
+                sub_network_identifier: None
+            },
             public_key: PublicKey {
                 hex_bytes: "29bdea325f58cb4ad7493ba7bc12c36bafb381350f5fbea0357ad2b869793e95".to_string(),
                 curve_type: CurveType::Edwards25519
             }
         };
-        let response = construction_derive_request(request, default_options()).await.unwrap();
+
+        let server_options = Options {
+            iota_endpoint: "https://api.lb-0.testnet.chrysalis2.com".to_string(),
+            network: "testnet6".to_string(),
+            mode: "online".to_string(),
+            port: 3030
+        };
+
+        let response = construction_derive_request(request, server_options).await.unwrap();
         assert_eq!("atoi1qqdxdqak4x96hzw6rkt48t4x5t84e209jmmlkp6t6lcx8aj6unr7zzv994h", response.account_identifier.address)
     }
 
     #[tokio::test]
-    #[should_panic]
     async fn test_bad_network() {
         let request = ConstructionDeriveRequest {
             network_identifier: NetworkIdentifier {
@@ -99,7 +93,15 @@ mod tests {
             }
         };
 
-        let _ = construction_derive_request(request, default_options()).await.unwrap();
-    }
+        let server_options = Options {
+            iota_endpoint: "https://api.lb-0.testnet.chrysalis2.com".to_string(),
+            network: "testnet6".to_string(),
+            mode: "online".to_string(),
+            port: 3030
+        };
 
+        if let Ok(_) = construction_derive_request(request, server_options).await {
+            panic!("expected bad network error")
+        }
+    }
 }
