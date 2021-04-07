a66284320b99264964673d6b8241fbda83a7698f
rusfam <samuel.rufinatscha@iota.org>
2021-04-07 21:24:14 +0200

    create check_data_testnet.sh

--- README.md
+++ README.md
@@ -15,7 +15,8 @@
 
  - Implementation of both Data API and Construction API
  - Stateless, offline, curve-based transaction construction
- 
+ - [Storage Pruning](https://www.rosetta-api.org/docs/storage_pruning.html#docsNav) using a block height-based configuration setting
+  
 ## Quick start
 
 Ensure `docker` and `docker-compose` is installed. As specified in the Rosetta API documentation, all Rosetta implementations must be deployable via Docker and support running via either an online or offline mode.
@@ -34,10 +35,18 @@ ROSETTA_BECH32_HRP=atoi ROSETTA_NETWORK_NAME=testnet7 ROSETTA_INDEXATION=Rosetta
 ```
 
 ## Testing with rosetta-cli
-To validate `rosetta-iota`, [install `rosetta-cli`](https://github.com/coinbase/rosetta-cli#install)
-and run one of the following commands:
-* `rosetta-cli check:data --configuration-file rosetta-cli-conf/devnet.json`
-* `rosetta-cli check:construction --configuration-file rosetta-cli-conf/devnet.json`
+Ensure the IOTA node is running and an instance of the Rosetta API is available.
+To validate the correctness of `rosetta-iota`, [install `rosetta-cli`](https://github.com/coinbase/rosetta-cli#install) and run the commands below.
+
+Testing the Data API **(Testnet)**:
+```
+./check_data_testnet.sh
+```
+
+Testing the Construction API **(Testnet)**:
+```
+./check_construction_testnet.sh
+```
  
 ## Documentation
 
---
+++ check_data_testnet.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# define a few vars
+DATA_DIR=".rosetta-cli-testnet"
+ROOT=$(pwd)
+CONF_DIR=$ROOT/rosetta-cli-conf/testnet
+
+# 1 to enable, comment out to disable
+#BOOTSTRAP_BALANCES=1 ...deletes the DATA_DIR, downloads the latest available IOTA snapshot and starts synching from the snapshot state
+#NO_BOOTSTRAP=1 ...continues to synch where it ended last time (DATA_DIR must exist and the ledger state must be present)
+
+if [ -z "$BOOTSTRAP_BALANCES" ] && [ -z "$NO_BOOTSTRAP" ]; then
+  echo "bootstrapping method not specified..."
+  exit 1
+fi
+
+# bootstrap balances
+if [ $BOOTSTRAP_BALANCES ]; then
+  # remove the data directory
+  rm -rf $DATA_DIR
+
+  # download latest snapshot and create the bootstrap_balances.json
+  RUST_BACKTRACE=1 cargo run -p rosetta-iota-utils -- --mode snapshot 2> /dev/null
+
+  # move generated file to $CONF_DIR
+  mv bootstrap_balances.json $CONF_DIR
+
+  SEP_INDEX=$(cat sep_index)
+  START_MS=`expr $SEP_INDEX + 1`
+
+  cat <<< $(jq --argjson START_MS "$START_MS" '.data.start_index |= $START_MS' $CONF_DIR/rosetta-iota.json) > $CONF_DIR/rosetta-iota.json
+
+  # clean up artifacts
+  rm delta_snapshot.bin
+  rm full_snapshot.bin
+  rm sep_index
+fi
+
+# start synching from $DATA_DIR
+if [ "$NO_BOOTSTRAP" ]; then
+  cat <<< $(jq 'del(.data.start_index)' $CONF_DIR/rosetta-iota.json) > $CONF_DIR/rosetta-iota.json
+  cat <<< $(jq 'del(.data.bootstrap_balances)' $CONF_DIR/rosetta-iota.json) > $CONF_DIR/rosetta-iota.json
+  if [ -d "$DATA_DIR" ]; then
+    echo "can not find data directory, please boostrap rosetta-cli..."
+  fi
+fi
+
+# test Data API
+echo "--------------------------------------------------------------------------------"
+echo "running rosetta-cli check:data"
+./rosetta-cli check:data --configuration-file $CONF_DIR/rosetta-iota.json
+DATA_EXIT=$?
+
+if [ $DATA_EXIT -ne 0 ]; then
+  echo "rosetta-cli check:data unsuccessful..."
+  exit 1
+fi
