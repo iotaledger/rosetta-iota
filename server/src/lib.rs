df8a3fd86ebde4ba53d2b30ddcfadec5a8e460d4
rusfam <samuel.rufinatscha@iota.org>
2021-04-08 19:07:57 +0200

    #![warn(missing_docs)]

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright 2020 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+#![warn(missing_docs)]
+
 use crate::{error::ApiError, options::RosettaMode, types::NetworkIdentifier};
 
 pub use options::Options;
--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright 2021 IOTA Stiftung
 // SPDX-License-Identifier: Apache-2.0
 
+#![warn(missing_docs)]
+
 pub use options::Options;
 
 pub mod consts;
