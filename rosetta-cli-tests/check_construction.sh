77d4110b93ffb88b0bd6a699e556713e79884e20
rufsam <samuel.rufinatscha@iota.org>
2021-10-29 09:45:27 +0200

    change "testnet7" to "chrysalis-devnnet"

--- rosetta-cli-tests/check_construction.sh
+++ rosetta-cli-tests/check_construction.sh
@@ -2,29 +2,29 @@
 
 CHRYSALIS_MAINNET_CONF_DIR="../rosetta-cli-conf/chrysalis-mainnet"
 CHRYSALIS_MAINNET_BECH32_HRP="iota"
-TESTNET7_CONF_DIR="../rosetta-cli-conf/testnet7"
-TESTNET7_HRP="atoi"
+CHRYSALIS_DEVNET_CONF_DIR="../rosetta-cli-conf/chrysalis-devnet"
+CHRYSALIS_DEVNET_BECH32_HRP="atoi"
 DB="rosetta-cli-db"
 
 # uncomment to enable
 # INSTALL_ROSETTA_CLI=1 ...installs rosetta-cli to the current folder
 # BOOTSTRAP_BALANCES=1 ...deletes the rosetta-cli database, downloads the latest available IOTA snapshots and bootstraps balances
 # NO_BOOTSTRAP=1 ...keeps the rosetta-cli database and continues synching from the available state
-# NETWORK = ...the network that should be tested on; can be either chrysalis-mainnet or testnet7
+# NETWORK = ...the network that should be tested on; can be either chrysalis-mainnet or chrysalis-devnet
 
 if [ -z "$NETWORK" ]; then
-  echo "Please specify the network on which to test. Following networks are supported: chrysalis-mainnet or testnet7."
+  echo "Please specify the network on which to test. Following networks are supported: chrysalis-mainnet or chrysalis-devnet."
   exit 1
 fi
 
 if [[ "$NETWORK" == "chrysalis-mainnet" ]]; then
   CONF_DIR=$CHRYSALIS_MAINNET_CONF_DIR
   HRP=$CHRYSALIS_MAINNET_BECH32_HRP
-elif [[ "$NETWORK" == "testnet7" ]]; then
-  CONF_DIR=$TESTNET7_CONF_DIR
-  HRP=$TESTNET7_HRP
+elif [[ "$NETWORK" == "chrysalis-devnet" ]]; then
+  CONF_DIR=$CHRYSALIS_DEVNET_CONF_DIR
+  HRP=$CHRYSALIS_DEVNET_BECH32_HRP
 else
-  echo "The provided network is not supported. Please run the test for following networks: chrysalis-mainnet or testnet7."
+  echo "The provided network is not supported. Please run the test for following networks: chrysalis-mainnet or chrysalis-devnet."
   exit 1
 fi
 
@@ -47,9 +47,9 @@ if [ $BOOTSTRAP_BALANCES ]; then
   if [[ "$NETWORK" == "chrysalis-mainnet" ]]; then
     echo "Copy snapshot(s) from ../data/snapshots/chrysalis-mainnet"
     cp -r ../data/snapshots/chrysalis-mainnet/* .
-  elif [[ "$NETWORK" == "testnet7" ]]; then
-    echo "Copy snapshot(s) from ../data/snapshots/testnet7"
-    cp -r ../data/snapshots/testnet7/* .
+  elif [[ "$NETWORK" == "chrysalis-devnet" ]]; then
+    echo "Copy snapshot(s) from ../data/snapshots/chrysalis-devnet"
+    cp -r ../data/snapshots/chrysalis-devnet/* .
   fi
 
   # create the bootstrap_balances.json file
