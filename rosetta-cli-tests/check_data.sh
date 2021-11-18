1c3ee1c552d896b50ed42de8d102696d590b8e81
rufsam <samuel.rufinatscha@iota.org>
2021-11-18 14:15:39 +0100

    refactor check_data.sh

--- rosetta-cli-tests/check_data.sh
+++ rosetta-cli-tests/check_data.sh
@@ -6,11 +6,6 @@ CHRYSALIS_MAINNET_BECH32_HRP="iota"
 CHRYSALIS_DEVNET_BECH32_HRP="atoi"
 DB_PATH="rosetta-cli-db"
 
-if [ -z "$NETWORK" ]; then
-  echo "please specify the network for which you want to test: chrysalis-mainnet or chrysalis-devnet"
-  exit 1
-fi
-
 if [[ "$NETWORK" == "chrysalis-mainnet" ]]; then
   CONF_DIR=$CHRYSALIS_MAINNET_CONF_DIR
   HRP=$CHRYSALIS_MAINNET_BECH32_HRP
@@ -18,11 +13,11 @@ elif [[ "$NETWORK" == "chrysalis-devnet" ]]; then
   CONF_DIR=$CHRYSALIS_DEVNET_CONF_DIR
   HRP=$CHRYSALIS_DEVNET_BECH32_HRP
 else
-  echo "the provided network is not supported; you can test for: chrysalis-mainnet or chrysalis-devnet"
+  echo "please specify the network for which you want to test: you can test for chrysalis-mainnet or chrysalis-devnet"
   exit 1
 fi
 
-# in case some rosetta-cli database already exists, remove it
+# in case some old rosetta-cli database already exists, remove it
 rm -rf $DB_PATH
 
 echo "copy node snapshots..."
@@ -32,7 +27,6 @@ elif [[ "$NETWORK" == "chrysalis-devnet" ]]; then
   cp -r ../data/snapshots/chrysalis-devnet/* .
 fi
 
-# generate the bootstrap_balances.json file
 echo "generate the bootstrap_balances.json file from the node snapshots..."
 RUST_BACKTRACE=1 cargo run -p rosetta-iota-snapshot --release -- --bech32-hrp $HRP
 ROSETTA_UTILS_EXIT=$?
@@ -41,7 +35,7 @@ if [ $ROSETTA_UTILS_EXIT -ne 0 ]; then
   exit 1
 fi
 
-# move bootstrap_balances file to $CONF_DIR
+# move bootstrap_balances file to the specific rosetta-cli-conf directory
 mv bootstrap_balances.json $CONF_DIR
 
 SEP_INDEX=$(cat sep_index)
@@ -54,7 +48,6 @@ rm delta_snapshot.bin
 rm full_snapshot.bin
 rm sep_index
 
-# installing rosetta-cli
 echo "installing rosetta-cli..."
 curl -sSfL https://raw.githubusercontent.com/coinbase/rosetta-cli/master/scripts/install.sh | sh -s -- -b .
 INSTALL_ROSETTA_CLI_EXIT=$?
@@ -64,12 +57,11 @@ if [ $INSTALL_ROSETTA_CLI_EXIT -ne 0 ]; then
   exit 1
 fi
 
-# test Data API
 echo "running rosetta-cli check:data"
 ./rosetta-cli check:data --configuration-file $CONF_DIR/config.json
-DATA_EXIT=$?
+CHECK_DATA_EXIT=$?
 
-if [ $DATA_EXIT -ne 0 ]; then
+if [ $CHECK_DATA_EXIT -ne 0 ]; then
   echo "rosetta-cli check:data unsuccessful"
   exit 1
 fi
