d5068ef9ccf98893bc6ada5fcfa3d5e9a62c5334
rufsam <samuel.rufinatscha@iota.org>
2021-11-29 12:21:37 +0100

    remove unused code

--- rosetta-iota-server/tests/data/block/block_handler.rs
+++ rosetta-iota-server/tests/data/block/block_handler.rs
@@ -1,12 +1,3 @@
-use crate::{
-    config::{VALID_BLOCKCHAIN, VALID_NETWORK}
-};
-
-use rosetta_iota_server::{
-    data::block::block_handler::BlockRequest,
-    types::{NetworkIdentifier, PartialBlockIdentifier},
-};
-
 use serial_test::serial;
 
 #[tokio::test]
